9d7c36b5842321fd50bc844715352e38594f934d
Jose Felix Soler Martinez <jfsm1997@gmail.com>
2017-11-21 10:50:15 +0100

    creacion del hook para asignar el propietario a la nueva lista

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = function(Listafamiliar) {
-
+    Listafamiliar.beforeRemote('create', function (context, Listafamiliar, next) {
+        
+        context.args.data.CoordinadorId = context.req.accessToken.userId;
+        next();
+    });
 };
