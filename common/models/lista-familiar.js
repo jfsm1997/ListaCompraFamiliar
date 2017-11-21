4c7346b672fc802e5db35e51cb430fe2b8db0adb
Jose Felix Soler Martinez <jfsm1997@gmail.com>
2017-11-21 11:12:50 +0100

    Creación del hook que asigna una lista a un usuario

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -6,4 +6,17 @@ module.exports = function(Listafamiliar) {
         context.args.data.CoordinadorId = context.req.accessToken.userId;
         next();
     });
+    Listafamiliar.afterRemote('create', function (context, Listafamiliar, next) {
+        //añade el id de la lista al usuario
+       var Usuario = Listafamiliar.app.models.Usuario;
+        Usuario.findById(context.req.accessToken.userId,function(err,usuario){ 
+        
+       usuario.listaFamiliarId= Listafamiliar.id;
+       usuario.save(function (err){
+        next();
+       });
+        });
+        
+    });
+    
 };
