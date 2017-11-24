a9bcb5fbd2459afe77c5670a168e5dceed35b833
Jose Felix Soler Martinez <jfsm1997@gmail.com>
2017-11-24 08:12:59 +0100

    Creación del metodo remoto (solicitudes)

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -1,22 +1,37 @@
 'use strict';
 
-module.exports = function(Listafamiliar) {
+module.exports = function (Listafamiliar) {
     Listafamiliar.beforeRemote('create', function (context, Listafamiliar, next) {
-        
+
         context.args.data.CoordinadorId = context.req.accessToken.userId;
         next();
     });
-    Listafamiliar.afterRemote('create', function (context, Listafamiliar, next) {
+    Listafamiliar.afterRemote('create', function (context, listafamiliar, next) {
         //añade el id de la lista al usuario
-       var Usuario = Listafamiliar.app.models.Usuario;
-        Usuario.findById(context.req.accessToken.userId,function(err,usuario){ 
-        
-       usuario.listaFamiliarId= Listafamiliar.id;
-       usuario.save(function (err){
-        next();
-       });
+        var Usuario = Listafamiliar.app.models.Usuario;
+        Usuario.findById(context.req.accessToken.userId, function (err, usuario) {
+
+            usuario.listaFamiliarId = listafamiliar.id;
+            usuario.save(function (err) {
+                next();
+            });
         });
-        
+
     });
-    
+    Listafamiliar.prototype.solicitar = function (context, callback) {
+        var solicitud;
+        var id =this.id;
+        var usuario=context.req.accessToken.userId;
+        this.Solicitud.add(usuario, function(err) {
+                solicitud={
+                    listaFamiliarId:id,
+                    usuarioId:usuario
+                    
+                }
+                        callback(null, solicitud);
+
+});
+    };
+
+
 };
