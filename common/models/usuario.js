17d8890a8ad3497a80933a223fdd01a8b3271642
Jose Felix Soler Martinez <jfsm1997@gmail.com>
2017-11-29 09:42:49 +0100

    creacion del metodo aceptar solicitudes

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,5 +1,38 @@
 'use strict';
 
-module.exports = function(Usuario) {
+module.exports = function (Usuario) {
+    //mitad falta comprobar y el array
+    Usuario.prototype.aceptarSolicitud = function (context, callback) {
+        var listaFamiliarid;
+        var usuarioSolicitante = this;
+
+        Usuario.findById(context.req.accessToken.userId, function (err, usuarioAutenticado) {
+            if (err)
+                callback(err);
+            listaFamiliarid = usuarioAutenticado.listaFamiliarId;
+
+            usuarioSolicitante.listaFamiliarId = listaFamiliarid;
+            usuarioSolicitante.save(function (err) {
+
+            });
+            usuarioSolicitante.Solicitud.remove(listaFamiliarid,
+                    function (err) {
+
+                    });
+            Usuario.find({where: {listaFamilarId: listaFamiliarid}}, function (err, UsuariosLista) {
+                callback(err, UsuariosLista);
+            });
+
+        });
+
+
+
+
+
+
+
+
+    };
 
 };
+ 
