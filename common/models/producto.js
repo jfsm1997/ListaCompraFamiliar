e52a9d0d5249426be53fba09cdf018622178a455
Jose Felix Soler Martinez <jfsm1997@gmail.com>
2017-12-04 08:23:05 +0100

    creacion del metodo remoto comprar productos ++

--- common/models/producto.js
+++ common/models/producto.js
@@ -1,23 +1,44 @@
 'use strict';
 
-module.exports = function(Producto) {
+module.exports = function (Producto) {
 
-    Producto.limpiarLista = function(context, callback) {
-    var array;
-    var usuario=Producto.app.models.Usuario;
-    var idlista;
-     usuario.findById(context.req.accessToken.userId, function (err, usuarioAutenticado) {
-            if (err)callback(err);
-            idlista=usuarioAutenticado.listaFamiliarId;
-            Producto.updateAll({comprar: 0}, function(err, info) {
+    Producto.limpiarLista = function (context, callback) {
+
+        var usuario = Producto.app.models.Usuario;
+        var idlista;
+        usuario.findById(context.req.accessToken.userId, function (err, usuarioAutenticado) {
+            if (err)
+                callback(err);
+            idlista = usuarioAutenticado.listaFamiliarId;
+            Producto.updateAll({listaFamilarId: idlista}, {comprar: 0}, function (err, info) {
                 Producto.find({where: {listaFamilarId: idlista}}, function (err, Lista) {
-                callback(err, Lista);
+                    callback(err, Lista);
+                });
             });
-    
-});
-           
         });
-   
-};
+    };
+    Producto.prototype.comprarProducto = function (context, callback) {
+        var usuario = Producto.app.models.Usuario;
+        var idlista;
+        var idproducto=this.id;
+        usuario.findById(context.req.accessToken.userId, function (err, usuarioAutenticado) {
+            
+            if (err)callback(err);
+            idlista = usuarioAutenticado.listaFamiliarId;
+            
+            Producto.findOne({where:{and:[{listaFamilarId: idlista},{id:idproducto}]}}, function (err, Producto) {
+              Producto.comprar=0;
+              Producto.save(function (err){
+                  Producto.find({where: {listaFamilarId: idlista}}, function (err, Lista) {
+                    callback(err, Lista);
+                });
+              });
+              
+
+            });
+            
+            
+         });
+    };
 
 };
--- common/models/usuario.js
+++ common/models/usuario.js
@@ -5,30 +5,44 @@ module.exports = function (Usuario) {
     Usuario.prototype.aceptarSolicitud = function (context, callback) {
         var listaFamiliarid;
         var usuarioSolicitante = this;
+        usuarioSolicitante.Solicitud(function(err,usuarioSo){
+           
+            if(usuarioSo.length!=0){
+                Usuario.findById(context.req.accessToken.userId, function (err, usuarioAutenticado) {
+            
+            if (err)callback(err);
+            
+             listaFamiliarid = usuarioAutenticado.listaFamiliarId;
+ 
+                usuarioSolicitante.listaFamiliarId = listaFamiliarid;
+                usuarioSolicitante.save(function (err) {
+                    Usuario.find({where: {listaFamilarId: listaFamiliarid}}, function (err, UsuariosLista) {
+                    callback(err, UsuariosLista);
+                });
 
-        Usuario.findById(context.req.accessToken.userId, function (err, usuarioAutenticado) {
-            if (err)
-                callback(err);
-            listaFamiliarid = usuarioAutenticado.listaFamiliarId;
+                });
+                usuarioSolicitante.Solicitud.remove(listaFamiliarid,
+                        function (err) {
 
-            usuarioSolicitante.listaFamiliarId = listaFamiliarid;
-            usuarioSolicitante.save(function (err) {
-
+                        });
+                
             });
-            usuarioSolicitante.Solicitud.remove(listaFamiliarid,
-                    function (err) {
-
-                    });
-            Usuario.find({where: {listaFamilarId: listaFamiliarid}}, function (err, UsuariosLista) {
-                callback(err, UsuariosLista);
-            });
-
+            }else{
+                callback(err,"este usuario no tiene solicitudes");
+            }
         });
+       
+
+        
+       
     };
     Usuario.prototype.rechazarSolicitud = function (context, callback) {
         var listaFamiliarid;
         var usuarioSolicitante = this;
-            
+        
+         usuarioSolicitante.Solicitud(function(err,usuarioSo){
+           
+            if(usuarioSo.length!=0){
         Usuario.findById(context.req.accessToken.userId, function (err, usuarioAutenticado) {
             if (err)
                 callback(err);
@@ -42,7 +56,10 @@ module.exports = function (Usuario) {
             });
 
         });
-       
+        }else{
+                callback(err,"este usuario no tiene solicitudes");
+            }
+        });
     };
 
 
